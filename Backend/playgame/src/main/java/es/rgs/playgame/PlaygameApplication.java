075507154fe0f732eb4f3d67267b0b1fa8749863
poyben <poybenglez@gmail.com>
2024-04-15 15:18:58 +0100

    Creado proyecto Spring

---
+++ Backend/playgame/src/main/java/es/rgs/playgame/PlaygameApplication.java
@@ -0,0 +1,13 @@
+package es.rgs.playgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaygameApplication.class, args);
+	}
+
+}
