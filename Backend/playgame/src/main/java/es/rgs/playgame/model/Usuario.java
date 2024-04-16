7369b5cda7bb6ad42c997f694faf79ff6cb9ff8a
poyben <poybenglez@gmail.com>
2024-04-16 15:31:53 +0100

    Hechos ficheros config

--- Backend/playgame/src/main/java/es/rgs/playgame/model/Usuario.java
+++ Backend/playgame/src/main/java/es/rgs/playgame/model/Usuario.java
@@ -1,5 +1,13 @@
 package es.rgs.playgame.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -18,7 +26,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-public class Usuario {
+public class Usuario implements UserDetails{
 	@Id
 	@GeneratedValue
 	private int id;
@@ -31,6 +39,37 @@ public class Usuario {
 	private Rol rol;
 	@ManyToOne
 	private Tienda tienda;
+	
+	
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<GrantedAuthority> authorities = new ArrayList<>();
+	    if (rol != null) {
+	        authorities.add(new SimpleGrantedAuthority(rol.getName()));
+	    }
+	    return authorities;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
 	  
 	
 
